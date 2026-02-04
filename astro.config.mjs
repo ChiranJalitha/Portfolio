7ad93302097618db29e3f659d85c6e3e30d770a0
Chiran Jalitha <chiranjalitha05@gmail.com>
2026-02-04 21:11:57 +0530

    Update Astro Config file  V.1.0.2
    Update Astro config for GitHub hosting services.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";  // if you have it
 
 export default defineConfig({
-  site: 'https://chiranjalitha.github.io',              // ← your GitHub username
+  site: 'https://chiranjalitha.github.io/Portfolio',              // ← your GitHub username
   base: '/Portfolio/',                                 // ← exact repo name with slashes!
   integrations: [tailwind()],                          // keep if you have Tailwind
   // ... any other config
